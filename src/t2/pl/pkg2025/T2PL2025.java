c229ce6e63537661584e97f83c1df8fd448b180e
Biyabylla <ardinanabila35@gmail.com>
2025-03-15 10:39:29 +0700

    Hello World!

--- src/t2/pl/pkg2025/T2PL2025.java
+++ src/t2/pl/pkg2025/T2PL2025.java
@@ -14,8 +14,8 @@ public class T2PL2025 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-
+        System.out.println("---------------Hello World!----------------");
+        
     }
     
 }
