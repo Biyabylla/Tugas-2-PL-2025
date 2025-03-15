5a9db99bd376656a85aca433c14e186eea8b8376
Biyabylla <ardinanabila35@gmail.com>
2025-03-15 10:54:25 +0700

    Tambah Nama dan NIM

--- src/t2/pl/pkg2025/T2PL2025.java
+++ src/t2/pl/pkg2025/T2PL2025.java
@@ -14,7 +14,10 @@ public class T2PL2025 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // TODO code application logic here
         System.out.println("---------------Hello World!----------------");
+        System.out.println("Nama\t: Nabila Putri Atdina");
+        System.out.print("NIM\t: 202457201055");
         
     }
     
